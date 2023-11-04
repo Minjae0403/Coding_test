e1cee5e17bf404a8e60e53a100221a530e8facdb
minjae <138181086+minjae0403@users.noreply.github.com>
2023-11-05 01:54:06 +0900

    Update 타겟 넘버.py
    생각 및 설명 추가

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,16 @@
 import itertools
 
+# 생각한 원리 : +, - 가 나올수 있는 모든 경우의 수를 꺼내서 대입으로 반복문을 짠다.
+
 def solution(numbers, target):
     answer = 0
     len_num = len(numbers)
+    # 아래가 +, - 를 이용해서 나올수 있는 모든 경우의 수를 구한것이다.
     pool = '+-'
     pool_list = list(itertools.product(pool,repeat = len_num))
+    # 반복문으로 모든 경우의 수를 계산해서 target과 같은지 판단한다.
     for i in pool_list:
         var_target = 0
-        # 아래 반복구문이 짧아진다.
         for j in range(len_num):
             if i[j] == '+':
                 var_target += numbers[j]
